0d517718297a8fd95fa33f865c127cd2ccece097
jdgarrido <damian.garrido.munoz@gmail.com>
2013-08-23 16:05:38 -0400

    adding abstract document

--- candideitorg/models.py
+++ candideitorg/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class CandideitorgDocument(models.Model):
+    remote_id = models.IntegerField()
+    resource_uri = models.CharField(max_length=255)
+
+    class Meta:
+	   abstract = True
--- candideitorg/tests.py
+++ candideitorg/tests.py
@@ -6,11 +6,32 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from candideitorg.models import CandideitorgDocument
+from django.core.management import call_command
 
+class CandideitorgDocumentTest(TestCase):
+    
+    def setUp(self):
+    	super(CandideitorgDocumentTest, self).setUp()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_candideitor_document_is_abstract(self):
+    	document = CandideitorgDocument()
+    	
+    	self.assertTrue(document)
+    	self.assertTrue(document._meta.abstract)
+
+    def test_candideitorg_document_attribute(self):
+
+    	class Element(CandideitorgDocument):
+    		class Meta:
+    			app_label = 'candideitorg'
+    	
+    	call_command('syncdb', verbosity=0)
+
+    	
+    	element = Element.objects.create(resource_uri="cualquiercosa", remote_id=2)
+
+    	self.assertEquals(element.resource_uri,'cualquiercosa')
+    	self.assertEquals(element.remote_id,2)
+
+    	
